6e7ebe98213f274683acd875dbe59d0c5e6a0930
mohosin2126 <mohosin212@gmail.com>
2023-08-15 01:21:05 +0800

    added event handler

---
+++ script.js
@@ -0,0 +1,10 @@
+// added event handler in submit button 
+document.getElementById('submit-btn').addEventListener('click',function(){
+    // email field
+    const usersEmail=document.getElementById('user-email');
+  const email=usersEmail.value ;
+    // password field 
+    const usersPassword=document.getElementById('user-password')
+    const password=usersPassword.value;
+    console.log('password')
+})
